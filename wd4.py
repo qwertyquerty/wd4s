16573f9aa092e08447a5e19252acb1ff739a4ecc
madeline <qwertytrogi@gmail.com>
2026-06-10 22:26:59 -0700

    change defatul stdev to 180s

--- wd4.py
+++ wd4.py
@@ -213,7 +213,7 @@ class Players(BaseModel):
 
         for other in Players.select().where(Players.id != self.id, Players.eliminated != 1):
             o = other.stats()
-            pm = p_a_beats_b((s["mean"] if s["mean"] else 100000, s["std"] or 1800), (o["mean"] if o["mean"] else 100000, o["std"] or 1800))
+            pm = p_a_beats_b((s["mean"] if s["mean"] else 100000, s["std"] or 180), (o["mean"] if o["mean"] else 100000, o["std"] or 180))
 
             if pm is None: return None
 
